f596ce29eba35f5817936fae41316dd56edf8611
Tim Vigneau <tjvig94@gmail.com>
2021-05-02 17:56:10 -0400

    Basic about  page works

--- src/components/Trait.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Trait() {
-
-}
-
-export default Trait;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { aboutMe } from '../portfolio-aboutme';
 
 function About() {
     return(
-        <h1>About Me</h1>
+        <main class="about-me">
+            <div class="container text-center">
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h1 id="about">This Is Me</h1>
+                    </div>
+                </div>
+                <div class="row">
+                {aboutMe.map(trait => {
+                    return(                                               
+                        <div class="col-lg-3 this-is-me">
+                            <h3>{trait.title}</h3>
+                            <p>{trait.desc}</p>
+                        </div>                   
+                    )               
+                })}    
+                </div>            
+            </div>
+        </main>
     )
 }
 
