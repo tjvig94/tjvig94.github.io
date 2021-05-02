41ca30980a73bffe882fc9cb4c9bb8a53662f8bb
Tim Vigneau <tjvig94@gmail.com>
2021-05-02 17:26:47 -0400

    Created routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Wrapper from './components/Wrapper';
 import About from './pages/About';
 import Homepage from './pages/Homepage';
 import Portfolio from './pages/Portfolio';
@@ -10,9 +9,15 @@ import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Router>
+        <Header />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Router>
     </div>
+    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
-
+    const location = useLocation();
+    return(
+        <ul className="nav justify-content-center mt-5 mb-5 px-3">
+            <li className="nav-item nav-link home-btn">
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item nav-link about-me-btn">
+                <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                    About Me
+                </Link>
+            </li>
+            <li className="nav-item nav-link portfolio-btn">
+                <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                    Portfolio
+                </Link>
+            </li>
+        </ul>
+    )    
 }
 
 export default Header;
