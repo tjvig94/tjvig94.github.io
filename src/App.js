2c7a893e22c431ab3d6a0d76ec86c5d82fe44e93
Tim Vigneau <tjvig94@gmail.com>
2021-05-02 16:43:09 -0400

    Created structure for imports/exports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
+import About from './pages/About';
+import Homepage from './pages/Homepage';
+import Portfolio from './pages/Portfolio';
+
 
 function App() {
   return (
